35bdd66751d652dc6b5fb98e5cf98bb9cbca5843
Idan Cohen <idan@idancohen.com>
2021-07-17 15:45:59 +0300

    Adding the Library to Firebase Cloud Functions

--- projects/functions/src/index.ts
+++ projects/functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from "firebase-functions";
+import { foo } from 'my-lib'; // Once more, it's super important this will point to the module and not the relative path. Make sure your IDE does this for you.
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest(
+  (request, response) => {
+    const value = foo();
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!" + value);
+  });
