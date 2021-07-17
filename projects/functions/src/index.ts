2ebd6eaaa818907e521d8411f9d26c6a9f9ba3c4
Idan Cohen <idan@idancohen.com>
2021-07-17 16:58:51 +0300

    implement types in cloud functions

--- projects/functions/src/index.ts
+++ projects/functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from "firebase-functions";
-import { foo } from 'my-lib'; // Once more, it's super important this will point to the module and not the relative path. Make sure your IDE does this for you.
+import { foo, MyClass, MyInterface } from 'my-lib'; // Once more, it's super important this will point to the module and not the relative path. Make sure your IDE does this for you.
 
 export const helloWorld = functions.https.onRequest(
   (request, response) => {
     const value = foo();
     functions.logger.info("Hello logs!", {structuredData: true});
+
+    const myClass = new MyClass();
+    functions.logger.info(myClass);
+
+    const myInterface: MyInterface = {
+      prop1: 2,
+      prop2: 'b'
+    };
+    functions.logger.info(myInterface);
+
     response.send("Hello from Firebase!" + value);
   });
