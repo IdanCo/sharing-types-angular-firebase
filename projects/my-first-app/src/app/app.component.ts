519c50827c754c51068e3e129a9d33f3577e5311
Idan Cohen <idan@idancohen.com>
2021-07-17 14:48:21 +0300

    Adding the Library to the Angular App

--- projects/my-first-app/src/app/app.component.ts
+++ projects/my-first-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { foo } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-app';
+
+  ngOnInit() {
+    const value = foo();
+    console.info(value);
+  }
 }
